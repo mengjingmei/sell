0564df60cbb7ab6e9965fe8005d294e4b43eb22f
mjm <1435974618@qq.com>
2018-06-16 13:17:21 +0800

    初始化外卖app工程项目

---
+++ src/components/tab/tab.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/goods">商品</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评价</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+      <router-view>哈哈哈</router-view>
+    </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display : flex
+      width : 100%
+      height : 40px
+      line-height : 40px
+      .tab-item
+        flex : 1
+        text-align : center
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/index.js';
+import resource from './resource/index.js';
+import './common/stylus/index.styl';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  resource,
+  components: {App},
+  template: '<App/>'
+});
